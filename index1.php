3a3081e78dc99342ca240e79bc0531435f6e2062
Nicole Goral <78056391+NicoleGoral@users.noreply.github.com>
2022-09-19 14:33:24 -0500

    Update index1.php

--- index1.php
+++ index1.php
@@ -1,23 +1,10 @@
+<?php require_once("header.php"); ?>
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-  </head>
-  <body>
-<table class="table table-striped">
-  <thead>
-    <tr>
-      <th>ID</th>
-      <th>Name</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php
+
+<?php
 $servername = "localhost";
-$username = "ngoralou_hwk3";
+$username = "ngoralou_Hwk3";
 $password = "Happygilmore1";
 $dbname = "ngoralou_MainDatabase";
 
@@ -26,38 +13,28 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
-}
+} 
 
-$sql = "SELECT courseID, coursePrefix, courseNumber, courseDescription from course";
+$sql = "SELECT courseID, coursePrefix, courseNumber, courseDescription FROM course";
 $result = $conn->query($sql);
+       
+ if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
 
-if ($result->num_rows > 0) {
-  // output data of each row
-  while($row = $result->fetch_assoc()) {
-?>
-  <tr>
-    <td><?=$row["courseID"]?></td>
-    <td><?=$row["coursePrefix"]?></td>
-    <td><?=$row["courseNumber"]?></td>
-    <td><?=$row["courseDescription"]?></td>
-  </tr>
-<?php
-  }
+
+        echo '<tr>
+                  <td scope="row">' . $row["courseID"]. '</td>
+                  <td>' . $row["coursePrefix"] .'</td>
+                  <td> '.$row["courseNumber"] .'</td>
+                  <td> '.$row["courseDescription"] .'</td>
+                   <br>
+                </tr>';
+    }
 } else {
-  echo "0 results";
-}
-$conn->close();
+    echo "0 results";
+} 
 ?>
-  </tbody>
-    </table>
-    <h1>Hello, world!</h1>
-    <form action="handlepost.php" method="get">
-Name: <input type="text" name="name"><br>
-E-mail: <input type="text" name="email"><br>
-<input type="submit">
-</form>
-    
-    <a class="btn btn-primary" href="page2.html" role="button">Page 2</a>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
-  </body>
-</html>
+       </tbody>
+    </div>
+</table>
